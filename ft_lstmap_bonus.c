511b3045bd2bdb2d6abffd6c4e8d36fa9335ca20
Ousama Barais <obarais@e1r13p4.1337.ma>
2024-11-04 17:58:22 +0100

    upb

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: obarais <obarais@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 14:56:57 by obarais           #+#    #+#             */
-/*   Updated: 2024/11/04 10:18:32 by obarais          ###   ########.fr       */
+/*   Updated: 2024/11/04 17:57:01 by obarais          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,18 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*list;
 	t_list	*new;
+	t_list	*content;
 
 	list = NULL;
 	if (del == NULL || f == NULL || lst == NULL)
 		return (NULL);
 	while (lst != NULL)
 	{
-		new = ft_lstnew(f(lst->content));
+		content = f(lst->content);
+		new = ft_lstnew(content);
 		if (new == NULL)
 		{
+			del(content);
 			ft_lstclear(&list, del);
 			return (NULL);
 		}
