09635373d6a3ae3fee3a1fe4a9b234fdddda92e5
Ousama Barais <obarais@e1r8p12.1337.ma>
2024-11-07 11:12:25 +0100

    jgh

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: obarais <obarais@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 14:47:19 by obarais           #+#    #+#             */
-/*   Updated: 2024/11/01 16:05:18 by obarais          ###   ########.fr       */
+/*   Updated: 2024/11/07 11:03:22 by obarais          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 	size_t	start;
 	size_t	end;
 
-	if (!s1 || !set)
+	if (s1 == NULL || set == NULL)
 		return (NULL);
 	len_s1 = ft_strlen(s1);
 	start = 0;
@@ -29,8 +29,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 	while (end > start && ft_strchr(set, s1[end - 1]) != NULL)
 		end--;
 	p = ft_substr(s1, start, end - start);
-	if (!p)
+	if (p == NULL)
 		return (NULL);
-	p[end - start] = '\0';
 	return (p);
 }
